456fec5f38765c4a67aea25fb914753713c877f1
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 09:23:41 +1300

    Added basic comments and renamed file
    Added basic comments to help beginners to understand aspects of this script.
    
    And also added 1.0 to the end of the file’s name as while editing this script i realised it needs to be improved and 2.0 will be releases soon

--- PiCamera Preview1.0.py
+++ PiCamera Preview1.0.py
@@ -1,10 +1,14 @@
+#import functions
 from picamera import PiCamera
 from time import sleep
+
+#set variable 
 camera = PiCamera()
+
+#show cameras view on screen
 camera.start_preview()
 sleep(2)
 camera.stop_preview()
-camera.capture('/home/pi/Desktop/image.jpg')
 
-
-        
+#take photo
+camera.capture('/home/pi/Desktop/image.jpg') 
